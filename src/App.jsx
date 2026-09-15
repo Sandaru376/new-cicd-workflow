1194df14ca24f874dcb3383380f90d5e20ba5a60
👨‍💻 Sandaru Malith Lakshan <135944062+Sandaru376@users.noreply.github.com>
2026-09-15 13:33:24 +0530

    Update app title

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
-    
+    <>
       <h1>cicd</h1>
-    
-  )
+      <p>Welcome to the CICD App</p>
+    </>
+  );
 }
 
-export default App
+export default App;
